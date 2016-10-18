af7f4265482edd41068a6109c9da021de1c76ab8
DSI\jnagy01 <johan.nagy.contact@gmail.com>
2016-10-18 09:46:29 +0200

    Spider Generation Basics 101 are ok. Ok.

---
+++ Assets/CustomizeLineRenderer.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CustomizeLineRenderer : MonoBehaviour {
+
+    LineRenderer lineRenderer;
+    public Vector3[] linePositions;
+
+	// Use this for initialization
+	void Start ()
+    {
+        
+
+
+    }
+	
+    public void SmoothCurve ()
+    {
+        lineRenderer = gameObject.GetComponent<LineRenderer>();
+
+        linePositions = Curver.MakeSmoothCurve(linePositions, 5);
+
+        lineRenderer.SetVertexCount(linePositions.Length);
+        int iterator = 0;
+        foreach (Vector3 position in linePositions)
+        {
+            lineRenderer.SetPosition(iterator, position);
+            iterator++;
+        }
+    }
+
+	// Update is called once per frame
+	void Update ()
+    {
+       
+	}
+
+
+}
---
+++ Assets/Spider_Skill_Displayer.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spider_Skill_Displayer : MonoBehaviour {
+
+    public int BranchNumbers;
+    Transform firstBranch;
+    public GameObject CompetencePrefab;
+    public GameObject SpiderWebWirePrefab;
+
+	// Use this for initialization
+	void Start ()
+    {
+        firstBranch = transform.FindChild("Competence");
+
+
+	    if (BranchNumbers < 4)
+        {
+            BranchNumbers = 4;
+        }
+
+        float BranchAngle = 360 / BranchNumbers;
+        float addAngle = BranchAngle;
+
+        Vector3 previousLineTip = Vector3.zero;
+
+        for (int i = 0; i < BranchNumbers; i++)
+        {
+            GameObject spawnedCompetence = GameObject.Instantiate(CompetencePrefab, Vector3.zero, Quaternion.identity) as GameObject;
+            spawnedCompetence.transform.SetParent(transform);
+            spawnedCompetence.transform.name = "Competence" + i;
+
+            Vector3 newRotatedVector = new Vector3(0, 20, 0);
+            newRotatedVector = Quaternion.AngleAxis(-addAngle, Vector3.forward) * newRotatedVector;
+
+            Vector3[] newPositions = new Vector3[2];
+            newPositions[0] = Vector3.zero;
+            newPositions[1] = newRotatedVector;
+
+            LineRenderer CompetenceLine = spawnedCompetence.GetComponent<LineRenderer>();
+            CompetenceLine.SetPositions(newPositions);
+
+            addAngle -= BranchAngle;
+            
+            if (Mathf.IsPowerOfTwo (i+1))
+            {
+                Debug.Log("Yup, peer number spawned! Hooray, let's celebrate!");
+                GameObject spawnedWebWire = GameObject.Instantiate(SpiderWebWirePrefab, Vector3.zero, Quaternion.identity) as GameObject;
+
+                Vector3[] webWirePos = new Vector3[3];
+                webWirePos[0] = previousLineTip;
+                webWirePos[1] = Vector3.zero;
+                webWirePos[2] = newPositions[1];
+
+                spawnedWebWire.GetComponent<CustomizeLineRenderer>().linePositions = webWirePos;
+                spawnedWebWire.GetComponent<CustomizeLineRenderer>().SmoothCurve();
+                //spawnedWebWire.GetComponent<LineRenderer>().SetPositions(webWirePos);
+            }
+
+            previousLineTip = newPositions[1];
+        }
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	    
+	}
+}
