12b5513feba2ce75d6e994c00b2ec5818271a48e
DSI\jnagy01 <johan.nagy.contact@gmail.com>
2016-10-19 17:04:37 +0200

    [WIP] Board base working

---
+++ Assets/BoardStep.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BoardStep : MonoBehaviour {
+
+    SpriteRenderer spriteRenderer;
+    bool PawnOverThis = false;
+
+	// Use this for initialization
+	void Start ()
+    {
+        spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+	
+	}
+
+    void OnMouseOver ()
+    {
+        if(!PawnOverThis)
+            spriteRenderer.color = Color.yellow;
+    }
+
+    void OnMouseExit ()
+    {
+        if(!PawnOverThis)
+            spriteRenderer.color = Color.white;
+    }
+
+    void OnTriggerStay2D (Collider2D hit)
+    { 
+        Debug.Log ("Something's on me, SEND HELP!!! " + hit.name);
+        if (hit.name == "Pawn")
+        {
+            spriteRenderer.color = Color.red;
+            PawnOverThis = true;
+        }
+    }
+
+    void OnTriggerExit2D (Collider2D hit)
+    {
+        if (hit.name == "Pawn")
+        {
+            spriteRenderer.color = Color.white;
+            PawnOverThis = false;
+        }
+    }
+}
---
+++ Assets/Board_PlayerPawn.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Board_PlayerPawn : MonoBehaviour {
+
+    public GameObject startStep;
+
+    Vector3 targetPosition;
+
+	// Use this for initialization
+	void Start ()
+    {
+        if (startStep == null)
+            Debug.LogError("First Step is missing in the pawn script! Please link the first step GameObject to the pawn.");
+        else
+        {
+            //Place the pawn on the first step
+            transform.position = startStep.transform.position;
+        }
+
+        targetPosition = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	    if (Input.GetMouseButtonDown (0))
+        {
+            targetPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        }
+
+        targetPosition.z = 0; //Kill the Z to avoid weird stuff in 2D
+        transform.position = Vector3.Lerp(transform.position, targetPosition, Time.deltaTime * 3);
+    }
+}
