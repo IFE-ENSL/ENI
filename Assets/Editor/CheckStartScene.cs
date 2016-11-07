3a86d85c1420c41d0f81bfb9a8bb7068452826e7
DSI\jnagy01 <johan.nagy.contact@gmail.com>
2016-11-07 15:19:22 +0100

    [Stable - Playable] Now possible to toggle the force login each time we enter PlayMode in Unity Editor

--- Assets/Editor/CheckStartScene.cs
+++ Assets/Editor/CheckStartScene.cs
@@ -12,11 +12,26 @@ public class CheckStartScene {
         EditorApplication.playmodeStateChanged += CheckScene;
     }
 
+    [MenuItem("Custom Tools/ Toggle force Login before entering PlayMode")]
+    public static void ToggleCheckScene ()
+    {
+        if (EditorPrefs.GetBool ("enableCheckStartScene") )
+        {
+            EditorPrefs.SetBool ("enableCheckStartScene", false);
+            Debug.Log("Force Login now deactivated");
+        }
+        else
+        {
+            EditorPrefs.SetBool("enableCheckStartScene", true);
+            Debug.Log("Force Login now activated");
+        }
+    }
+
     private static void CheckScene()
     {
         Debug.Log("Changed play state");
 
-        if(SceneManager.GetActiveScene().name != "Login" && EditorApplication.isPlaying)
+        if(EditorPrefs.GetBool("enableCheckStartScene") && SceneManager.GetActiveScene().name != "Login" && EditorApplication.isPlaying)
         {
             SceneManager.LoadScene("Login");
             Debug.ClearDeveloperConsole();
